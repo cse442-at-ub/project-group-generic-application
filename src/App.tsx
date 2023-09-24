0cfbc6c4004658b9e0fa17a8e9f789e3110709ba
m1lkman896 <michaelhorvath896@gmail.com>
2023-09-24 18:29:46 -0400

    ✨ Create Login Route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +30,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Router>
+        <Route path="/login" element={<LoginPage />} />
+     </Router>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import LoginPage from "./pages/LoginPage"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage/>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
+
+
