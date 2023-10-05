d564393765a17d1f7b41fedb7c883c10f3991b12
Bert Li <bertli@buffalo.edu>
2023-10-05 15:16:53 -0400

    See if it connects to post

--- src/backend/Loginbackend test.php
+++ src/backend/Loginbackend test.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
+echo "success";
+echo "success";
  //Check if the user is already logged in
-if (isset($_SESSION['username'])) {
+/*if (isset($_SESSION['username'])) {
     header("Location: dashboard.php");
     echo "already connected";
     exit;
-}
+}*/
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     include 'dbconnect.php';
