81a28029f15892e22476a3c60bdf8e2811cf1c61
Jonah <105020079+jonahwhat@users.noreply.github.com>
2023-10-16 16:34:10 -0400

    Add Professor's Function Page (desktop/mobile)
    Also includes minor fixes to the way the attendance code is generated

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.css'
+//import 'bootstrap/dist/css/bootstrap.css'
 import LoginPage from "./pages/LoginPage"
 import {
   createBrowserRouter,
