b4c585ed5fd5d64240e67706a2c5e31153724af0
bchow3 <98728043+bchow3@users.noreply.github.com>
2023-10-20 07:23:52 -0400

    Update attendanceinput.php with syntax fixes and fixed retrieving date from db

--- src/backend/attendanceinput.php
+++ src/backend/attendanceinput.php
@@ -3,36 +3,37 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
     session_start();
 
-    if ($_SERVER['REQUESET_METHOD'] === 'POST'){
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
         include 'dbconnect.php';
-        $username = "default";
-        if(!isset($_SESSION['username'])){
+        $username = "bchow3";//Current username will change when session is saved across pages
+        /*if(!isset($_SESSION['username'])){
             header("Location: Loginbackend.php");
             exit();
-        }
+        }*/
         $sql = "Select * from users  where username='$username'";
         $result = mysqli_query($conn, $sql);
         $row = mysqli_fetch_assoc($result);
         $classToken = $row['class token'];
         $currentDate = date('Y-m-d');
-        $username = $_SESSION['username'];
+        //$username = $_SESSION['username'];
         $attendanceCode = $_POST["code"];
-        $sql = "Select * from attendanceCode where code='$attendaceCode'";
+        mysqli_free_result($result);
+        $sql = "Select * from AttendanceCode where code='$attendanceCode'";
         $result = mysqli_query($conn, $sql);
+        $num = mysqli_num_rows($result);
         $row = mysqli_fetch_assoc($result);
         $timestamp = $row['date'];
-        $class = $row['class token']
+        $class = $row['class token'];
         $dbDate = date("Y-m-d", strtotime($timestamp));
-        $num = mysqli_num_rows($result);
-
-        if($num == 1 && $dbDate === $currentDate && $classToken === $class){
-            $sql = "INSERT INTO Attendance (username, `class token`)
-                Values ('$username', '$classToken')";
+       if($num == 1 && $dbDate === $currentDate && $classToken === $class){
+           $sql = "INSERT INTO Attendance (username, `class token`)
+             VALUES ('$username', '$classToken')";
             $result = mysqli_query($conn, $sql);
-            echo "insertion success";
+           echo "insertion success";
         } else{
             echo "invalid code";
         }
+        //echo "invalid code";
     }
     
 ?>
