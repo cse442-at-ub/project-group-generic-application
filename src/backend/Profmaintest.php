1499e061639a9da238df044a9b39ad36c2882042
Bert Li <bertli@buffalo.edu>
2023-10-29 18:21:00 -0400

    Removed need for current_time

--- src/backend/Profmaintest.php
+++ src/backend/Profmaintest.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 include 'dbconnect.php';
-$current_time = date("Y-m-d H:i:s");
+//$current_time = date("Y-m-d H:i:s");
 $Attendcode = '12345';
 $classToken = '123456';
 //if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $sql = "INSERT INTO 'AttendanceCode' ( 'code', 'classtoken', 'date') VALUES ('$Attendcode', '$classToken', '$current_time')";
+    $sql = "INSERT INTO 'AttendanceCode' ( 'code', 'classtoken', 'date') VALUES ('$Attendcode', '$classToken', current_timestamp())";
     $result2 = mysqli_query($conn, $sql);
     $passwordtocompare = "SELECT username FROM Attendance WHERE 'classtoken' = '$classToken'";
     $result = mysqli_query($conn, $passwordtocompare);
