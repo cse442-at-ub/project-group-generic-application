8628d21cc390184cc216f7d421fc97448fe5c66f
Bert Li <bertli@buffalo.edu>
2023-10-29 18:12:40 -0400

    removed need for post in profmaintest

--- src/backend/Profmaintest.php
+++ src/backend/Profmaintest.php
@@ -4,14 +4,14 @@ include 'dbconnect.php';
 $current_time = date("Y-m-d H:i:s");
 $Attendcode = '12345';
 $classToken = '123456';
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+//if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $sql = "INSERT INTO 'AttendanceCode' ( 'code', 'classtoken', 'date') VALUES ('$Attendcode', '$classToken', '$current_time')";
     $result2 = mysqli_query($conn, $sql);
     $passwordtocompare = "SELECT username FROM Attendance WHERE 'classtoken' = '$classToken'";
     $result = mysqli_query($conn, $passwordtocompare);
     $row = mysqli_fetch_assoc($result); // array used on frontend hopefully
     print_r($row);
-}
+//}
 $conn->close();
 ?>
 
