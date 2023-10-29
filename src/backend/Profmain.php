bb7b65a8d750dd6083286a483962d01dfccba7ff
Bert Li <bertli@buffalo.edu>
2023-10-29 17:52:47 -0400

    added a print value for profmain for ez debug

--- src/backend/Profmain.php
+++ src/backend/Profmain.php
@@ -10,6 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $passwordtocompare = "SELECT username FROM Attendance WHERE 'classtoken' = '$classToken'";
     $result = mysqli_query($conn, $passwordtocompare);
     $row = mysqli_fetch_assoc($result); // array used on frontend hopefully
+    print_r($row);
 }
 $conn->close();
 ?>
--- src/backend/Profmaintest.php
+++ src/backend/Profmaintest.php
@@ -6,9 +6,11 @@ $classToken = $request->session()->get('class token');
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     include 'dbconnect.php';
     $sql = "INSERT INTO 'AttendanceCode' ( 'code', 'classtoken', 'date') VALUES ('$Attendcode', '$classToken', '$current_time')";
-    $result = mysqli_query($conn, $sql);
+    $result2 = mysqli_query($conn, $sql);
     $passwordtocompare = "SELECT username FROM Attendance WHERE 'classtoken' = '$classToken'";
-    $row = mysqli_fetch_assoc($result);
+    $result = mysqli_query($conn, $passwordtocompare);
+    $row = mysqli_fetch_assoc($result); // array used on frontend hopefully
+    print_r($row);
 }
 $conn->close();
 ?>
