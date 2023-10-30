9fe14c9a66047e928d6fd21cb6ec884238495c37
Bert Li <bertli@buffalo.edu>
2023-10-29 20:19:08 -0400

    further fixes on syntex

--- src/backend/Profmaintest.php
+++ src/backend/Profmaintest.php
@@ -8,7 +8,7 @@ $classToken = '123456';
     $sql = "INSERT INTO AttendanceCode ( code, `class token`) VALUES ('$Attendcode', '$classToken')";
     $result2 = mysqli_query($conn, $sql);
     echo "success2";
-    $passwordtocompare = "SELECT username FROM Attendance WHERE 'classtoken' = '$classToken'";
+    $passwordtocompare = "SELECT username FROM Attendance WHERE `class token` = '$classToken'";
     $result = mysqli_query($conn, $passwordtocompare);
     $row = mysqli_fetch_assoc($result); // array used on frontend hopefully
     print_r($row);
