66df1b86bc52f21874a5ba2af9480589133524e0
bchow3 <bchow3466@gmail.com>
2023-11-09 15:32:46 -0500

    fixed issue where csv file is blank and only download when class code is correct

--- src/backend/downloadAttendance.php
+++ src/backend/downloadAttendance.php
@@ -1,25 +1,26 @@
 <?php
-
+    ob_start();
     include 'dbconnect.php';
-
+    ob_end_clean();
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $classCode = $_POST['class_code'];
-        $sql = "SELECT * FROM Attendance WHERE `class token`= $classCode";
+        //$classCode = "ABCD";
+        $sql = $sql = "SELECT * FROM `Attendance` WHERE `class token`= \"$classCode\"";
         $result = mysqli_query($conn, $sql);
-        
-        $csvFileName = "attendace_report.csv";
-        $csvFile = fopen($csvFileName, "w");
-        fputcsv($csvFile, ["Username", "Class Code", "Date"]);
-
-        while ($row = mysqli_fetch_assoc($result)){
-            fputcsv($csvFile, [$row['username'], $row['class token'], $row['date']]);
+        $num = mysqli_num_rows($result);
+        if ($num > 1){
+            $csvFileName = "attendance_report.csv";
+            $data = array("username", "class code", "date");
+            header('Content-Type: application/csv');
+            header('Content-Disposition: attachment; filename="' . $csvFileName . '"');
+            $csvFile = fopen("php://output", "w");
+            fputcsv($csvFile, $data);
+            while ($row = mysqli_fetch_assoc($result)){
+                fputcsv($csvFile, [$row['username'], $row['class token'], $row['date']]);
+            }
+            fclose($csvFile);
         }
 
-        fclose($csvFile);
-
-        header('Content-Type: application/csv');
-        header('Content-Disposition: attachment; filename="' . $csvFileName . '"');
-
-        readfile($csvFileName);
+        //readfile($csvFileName);
     }
 ?>
