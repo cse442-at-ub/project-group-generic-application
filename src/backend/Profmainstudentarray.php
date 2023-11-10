2872fde811df990c3e157d21cacccb02d5a5bf3d
Bert Li <bertli@buffalo.edu>
2023-11-10 13:29:50 -0500

    added just the student array

--- src/backend/Profmainstudentarray.php
+++ src/backend/Profmainstudentarray.php
@@ -13,22 +13,18 @@ if (!isset($_SESSION['username'])) {
     $conn->close();
     exit;
 }
-
-//if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $username = $_SESSION['username'];
     echo "$username";
     $passwordtocompare4 = "SELECT `class token` FROM userSignup WHERE username = '$username'";
     $result5 = mysqli_query($conn, $passwordtocompare4);
     $row = mysqli_fetch_assoc($result5);
     $classToken = $row['class token'];
-    $sql = "INSERT INTO AttendanceCode ( code, `class token`) VALUES ('$Attendcode', '$classToken')";
-    $result2 = mysqli_query($conn, $sql);
-    echo "success2";
     $passwordtocompare = "SELECT username FROM Attendance WHERE `class token` = '$classToken'";
     $result = mysqli_query($conn, $passwordtocompare);
     $row = mysqli_fetch_all($result); // array used on frontend hopefully
     print_r($row);
     echo "success3";
-//}
+}
 $conn->close();
 ?>
