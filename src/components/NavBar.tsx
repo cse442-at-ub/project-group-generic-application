5d846f56e48cd770502a274f34290371d330bc5a
m1lkman896 <michaelhorvath896@gmail.com>
2023-11-14 14:59:15 -0500

    ⚡️ WIP NavBar Implemented on Profile Page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,29 +1,37 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
+//import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+//import IconButton from '@mui/material/IconButton';
+//import MenuIcon from '@mui/icons-material/Menu';
 
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
+        {/* <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
-          >
+          > 
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          </Typography>*/}
+          
+
+          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button color="inherit" component={Link} to="/main">Main</Button>
+          <Button color="inherit" component={Link} to="/login">Login</Button>
+          <Button color="inherit"component={Link} to="/signup">Signup</Button>
+          <Button color="inherit"component={Link} to="/profile">Profile</Button>
+          <Button color="inherit"component={Link} to="/function">Function</Button>
         </Toolbar>
       </AppBar>
     </Box>
