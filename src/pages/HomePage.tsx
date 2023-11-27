730331d87da3cceb8a5e3e10edd3d53be77fffa9
Jonah <jonahhoe@buffalo.edu>
2023-11-27 17:22:14 -0500

    Updates to homepage to include link to login

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,19 @@
 import '/src/App.css'
 import NavBar from '../components/NavBar';
+import { useNavigate } from 'react-router-dom';
 
 
 function HomePage() {
+
+    let navigate = useNavigate(); 
+    const routeChange = () =>{ 
+      let path = `./login`; 
+      navigate(path);
+    }
+    
     return (
         <>
-        <NavBar /><div className="mainDiv" style={{ padding: '6vh', margin: "auto" }}>
+        <NavBar /><div className="mainDiv" style={{ padding: '6vh', margin: "auto", top:"12%" }}>
 
             <h1 className="gradient-text" style={{ fontSize: 57, fontWeight: "bold", margin: 10, textAlign: 'center'}}>Generic Attendance</h1>
             <div style={{ fontSize: 22}}>
@@ -13,7 +21,7 @@ function HomePage() {
             
             </div>
             <div style={{ padding: "5vh", fontSize: 22, background: "linear-gradient(0.35turn,#ffffff63, #bfddff65)"}}>
-            <button className="button-5" role="button">Click here to get started!</button>
+            <button onClick={routeChange} className="button-5" role="button">Click here to get started!</button>
             
             </div>
             <br></br>
