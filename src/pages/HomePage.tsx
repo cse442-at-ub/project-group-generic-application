54eb2ac941b52ffed287e7b601f7296e903c46f9
Jonah <105020079+jonahwhat@users.noreply.github.com>
2023-11-27 16:38:21 -0500

    Homepage redesign

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,18 +4,19 @@ import NavBar from '../components/NavBar';
 
 function HomePage() {
     return (
-        <><NavBar /><div className="mainDiv" style={{ textAlign: 'center', padding: '4vh' }}>
+        <>
+        <NavBar /><div className="mainDiv" style={{ padding: '6vh', margin: "auto" }}>
 
-            <h1 style={{ fontSize: 50, fontWeight: "bold", margin: 10 }}>Links For Testing Purposes</h1>
-            <a style={{ fontSize: 20 }} href={"./#/main"}>Click here to go to the Main Page</a>
+            <h1 className="gradient-text" style={{ fontSize: 57, fontWeight: "bold", margin: 10, textAlign: 'center'}}>Generic Attendance</h1>
+            <div style={{ fontSize: 22}}>
+            Looking for a <b>better</b>, more <b>engaging</b> way to keep track of your classes attendance?
+            
+            </div>
+            <div style={{ padding: "5vh", fontSize: 22, background: "linear-gradient(0.35turn,#ffffff63, #bfddff65)"}}>
+            <button className="button-5" role="button">Click here to get started!</button>
+            
+            </div>
             <br></br>
-            <a style={{ fontSize: 20 }} href={"./#/login"}>Click here to go to the Login Page</a>
-            <br></br>
-            <a style={{ fontSize: 20 }} href={"./#/function"}>Click here to go to the Function Page</a>
-            <br></br>
-            <a style={{ fontSize: 20 }} href={"./#/signup"}>Click here to go to the Signup Page</a>
-            <br></br>
-            <a style={{ fontSize: 20 }} href={"./#/profile"}>Click here to go to the Profile Page</a>
 
         </div></>
     )
