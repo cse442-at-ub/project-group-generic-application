5247e3543d7017ce32be72022324146517ccc2de
Mcaunt <138386432+Mcaunt@users.noreply.github.com>
2023-11-30 17:52:56 -0500

    Finished click on joined class and carry class token to function page

--- src/pages/FunctionPage.tsx
+++ src/pages/FunctionPage.tsx
@@ -7,31 +7,36 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import axios from 'axios';
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 
 const FunctionPage = () => {
-  const [code, setcode] = useState('');
+  const [code, setCode] = useState('');
+  const location = useLocation();
+  const classToken = location.state?.classToken || 'DEFAULT_TOKEN';
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log({
         code,
+        classToken,
     });
 
     const codeRegex = /^[A-Za-z0-9]+$/;
 
     if (!codeRegex.test(code)) {
-      alert('Only capital, lowercase letters and numbers are allow');
+      alert('Only capital, lowercase letters and numbers are allowed');
       return;
-  }
+    }
 
     const attendanceCode = {
-        'code': code,
+      'code': code,
+      'classToken': classToken,
     };
     
     axios.post('https://www-student.cse.buffalo.edu/CSE442-542/2023-Fall/cse-442ab/attendanceinput.php', attendanceCode)
         .then(response => {
-            console.log('Data submitted successful', response.data);
+            console.log('Data submitted successfully', response.data);
         })
         .catch(error => {
             console.error('Error submitting data', error);
@@ -40,49 +45,49 @@ const FunctionPage = () => {
 
   return (
     <>
-    <NavBar />
-    <div className="mainDiv">
-    <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Typography component="h1" variant="h6">
-            Look up at Professor Screen for Code
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="code"
-              label="Attendance code"
-              name="code"
-              autoComplete="code"
-              autoFocus
-              value={code}
-              onChange={(e) => setcode(e.target.value)}
-            />
-            <Grid container>
-              <Grid item xs>
+      <NavBar />
+      <div className="mainDiv">
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Typography component="h1" variant="h6">
+              Look up at Professor Screen for Code
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="code"
+                label="Attendance code"
+                name="code"
+                autoComplete="code"
+                autoFocus
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+              />
+              <Grid container>
+                <Grid item xs>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Having problems? Contact Us"}
+                  </Link>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Having problems? Contact Us"}
-                </Link>
-              </Grid>
-            </Grid>
+            </Box>
           </Box>
-        </Box>
-      </Container>
+        </Container>
       </div>
-      </>
-  )
-}
+    </>
+  );
+};
 
-export default FunctionPage
+export default FunctionPage;
