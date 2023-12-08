d930bb1562f1f6a7fb6778b131aac26d7acad48c
bchow3 <98728043+bchow3@users.noreply.github.com>
2023-12-08 18:40:20 -0500

    Update downloadAttendance.php
    fixed issue of processing class code post request
    students should now only see their own attendance
    changed display of attendance to descending down from recent attendance for both professors and students

--- src/backend/downloadAttendance.php
+++ src/backend/downloadAttendance.php
@@ -1,17 +1,58 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Content-Type");
+    header('Content-Type: application/json');
+    session_start();
     ob_start();
     include 'dbconnect.php';
     ob_end_clean();
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $classCode = $_POST['class_code'];
-        //$classCode = "ABCD";
-        $sql = $sql = "SELECT * FROM `Attendance` WHERE `class token`= \"$classCode\"";
+	$data = json_decode(file_get_contents('php://input'), true);
+	$classCode = $data['class_code'];
+        //$classCode = $_POST['class_code'];
+        //$classCode = "ABDF";
+	$username = $_SESSION['username'];
+	//$username = $_POST['username'];
+	//$username = "bertli@buffalo.edu";
+	//$classCode = classCode[];
+
+	$sql = "Select * from class_token where username=\"$username\" AND code = \"$classCode\"";
+	$result = mysqli_query($conn, $sql);
+	echo mysqli_error($conn);
+	$row = mysqli_fetch_assoc($result);
+	$role = $row['role'];
+
+
+
+
+	/*$csvFileName = "attendance_reports.csv";
+            $data = array("username", "class code", "date", $username, $classCode);
+            header('Content-Type: text/csv');
+            header('Content-Disposition: attachment; filename="' . $csvFileName . '"');
+            $csvFile = fopen("php://output", "w");
+            fputcsv($csvFile, $data);
+		fputcsv($csvFile, $username);
+	 fclose($csvFile);*/
+
+
+
+
+
+
+	//echo $role;
+	if( $role == "Professor"){
+	  $sql = "SELECT * FROM `Attendance` WHERE `class token`= \"$classCode\" ORDER BY date DESC";
+	} else{
+		$sql = "SELECT * FROM `Attendance` WHERE `class token` = \"$classCode\" AND `username`  = \"$username\" ORDER BY date DESC";	    
+        }
+        //$sql = "SELECT * FROM `Attendance` WHERE `class token`= \"$classCode\" ORDER BY date ASC";
         $result = mysqli_query($conn, $sql);
+	echo mysqli_error($conn);
         $num = mysqli_num_rows($result);
-        if ($num > 1){
-            $csvFileName = "attendance_report.csv";
+        if ($num > 0){
+            $csvFileName = "attendance_reports.csv";
             $data = array("username", "class code", "date");
-            header('Content-Type: application/csv');
+            header('Content-Type: text/csv');
             header('Content-Disposition: attachment; filename="' . $csvFileName . '"');
             $csvFile = fopen("php://output", "w");
             fputcsv($csvFile, $data);
@@ -19,8 +60,12 @@
                 fputcsv($csvFile, [$row['username'], $row['class token'], $row['date']]);
             }
             fclose($csvFile);
+	    //echo "sucessful download";
         }
 
-        //readfile($csvFileName);
+        readfile($csvFileName);
     }
+    else{
+	echo "did not reach post";
+	}
 ?>
